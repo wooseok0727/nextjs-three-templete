00713bbcff9786ffdcb3df0f06e4a89de33fddc3
wooseok0727 <crash2@naver.com>
2022-04-24 04:38:26 +0900

    404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
-export const NotFount404 = () => {
+export default function NotFount404() {
   const router = useRouter();
 
   useEffect(() => {
@@ -9,4 +9,4 @@ export const NotFount404 = () => {
   }, [router]);
 
   return null;
-};
+}
